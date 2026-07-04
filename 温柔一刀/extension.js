76269fd23ea83c061fe0652363097382fbeabe11
潜水火火 <104590442+QCXYLTJ@users.noreply.github.com>
2026-07-04 19:24:15 +0800

    1

--- 温柔一刀/extension.js
+++ 温柔一刀/extension.js
@@ -281,9 +281,7 @@ const extensionInfo = await lib.init.promises.json(`extension/温柔一刀/info.
 game.import('extension', function (lib, game, ui, get, ai, _status) {
   const gentle = {
     name: '温柔一刀',
-    init() {
-      debugger;
-    },
+    init() { },
     arenaReady() {
       console.time('温柔一刀arenaReady');
       console.log(Object.keys(lib.skill).length, 'lib.arenaReady', 'skill');
